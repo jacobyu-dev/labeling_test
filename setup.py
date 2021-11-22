be9927679f011453be87a8053ac5b7398950be1e
jacobyu-dev <rdx1696@gmail.com>
2021-11-22 15:49:14 +0900

    release init2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='ypolaris_ai_labeling_tool',
     version='0.0.1',
     description='ypolaris image labeling tool',
-    url='',
+    url='https://github.com/jacobyu-dev/labeling_test.git',
     packages=['labeling_lib'],
     install_require=[
         'opencv-python>=4.5.1.48'
